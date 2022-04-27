93f638eacdcf729fdb45f22ba0c93ec200125562
Nam Nguyen <ubuntu@namnt-vnl.localdomain>
2022-04-27 19:24:45 +0700

    .

--- project-1/src/main.rs
+++ project-1/src/main.rs
@@ -1,3 +1,42 @@
+use std::io;
+
+// CRUD APPLICATION
+enum Manager {
+    AddStudent,
+    ViewStudent,
+    EditStudent,
+    DeleteStudent
+}
+
+fn get_input() -> Option<String> {
+    let mut buffer = String::new();
+    io::stdin().read_line(buf)
+}
+
+impl Manager {
+    fn show() {
+        println!("");
+        println!("== Manager Panel ==");
+        println!("");
+        println!("1. Add Student");
+        println!("2. View Students");
+        println!("3. Edit Student");
+        println!("4. Delete Student");
+        println!("");
+        println!("Please Enter Your Choice");
+        println!("");
+    }
+
+    fn choice(input: &str) -> Option<Manager> {
+        match input {
+            "1" => Some(Manager::AddStudent),
+            "2" => Some(Manager::AddStudent),            
+            "3" => Some(Manager::AddStudent),            
+            "4" => Some(Manager::AddStudent),            
+            _ => None
+        }
+    } 
+}
 fn main() {
-    println!("Hello, world!");
+    Manager::show();
 }
