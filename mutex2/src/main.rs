cc56095b9f7aa0dc32bfb316ca54eda760e8e9e6
Nam Nguyen <info.namnt@gmail.com>
2022-05-04 16:40:53 +0700

    mutex2

---
+++ mutex2/src/main.rs
@@ -0,0 +1,43 @@
+use parking_lot::{Mutex, ReentrantMutex};
+use std::{sync::Arc, rc::Rc, thread};
+use std::time::Duration;
+use backoff::ExponentialBackoff;
+
+type ArcAccount = Arc<Mutex<Account>>;
+
+struct Account {
+    balance: usize
+}
+
+fn transfer(from: ArcAccount, to: ArcAccount, amount: usize) {
+    let op = || {
+        if let Some(mut from) = from.try_lock() {
+            if let Some(mut to) = to.try_lock() {
+                from.balance -= amount;
+                to.balance += amount;
+                return Ok(());
+            }
+        }
+        Err(0) ?
+    };
+
+    // Tối ưu hóa thời gian sleep cho thread 
+    let backoff = ExponentialBackoff::default();
+    backoff::retry(backoff, op);
+}
+fn main() {
+    let a = Arc::new(Mutex::new(Account {balance:400}));
+    let b = Arc::new(Mutex::new(Account {balance:400}));
+
+    {
+    let transaction_1 = thread::spawn(move || {
+        transfer(a, b, 100);
+    });
+    }
+
+    {
+        let transaction_2 = thread::spawn(move || {
+            transfer(b, a, 300);
+        });
+    }
+}
