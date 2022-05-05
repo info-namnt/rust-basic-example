bfc7a1963c95a7b0b8bfbf475fc351c0202988e8
Nam Nguyen <info.namnt@gmail.com>
2022-05-05 17:26:43 +0700

    multithread server

---
+++ avoidObstacles/src/main.rs
@@ -0,0 +1,23 @@
+
+
+fn solution(inputArray: Vec<i32>) -> i32 {
+    //find step 
+    4
+}
+  
+
+fn main() {
+    println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod test {
+    use crate::solution;
+    #[test]
+    fn test1() {
+        let test1 = vec![5, 3, 6, 7, 9];
+        let expected = solution(test1);
+        let result = 4;
+        assert_eq!(&result, &expected, "Fail")
+    }
+}
