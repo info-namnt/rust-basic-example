a863aac3084a7074d76be79ed485b655a11df5f8
Nam Nguyen <info.namnt@gmail.com>
2022-05-06 00:01:16 +0700

    update

--- avoidObstacles/src/main.rs
+++ avoidObstacles/src/main.rs
@@ -1,14 +1,32 @@
-
-
 fn solution(inputArray: Vec<i32>) -> i32 {
-    //find step 
-    4
+ //largest step 
+ let mut max = inputArray.iter().max().unwrap() + 1;
+  
+ for n in 2..max {
+   //
+   let result = inputArray.iter().map(|x| x % n).min().unwrap();
+   
+   if 0 != result {
+     return n;
+   } 
+ }
+ max
 }
   
 
 fn main() {
-    println!("Hello, world!");
+    let inputArray = vec![5, 3, 6, 7, 9];
+    let mut jump = 0;
+    for n in 2.. {
+        let result = inputArray.iter().map(|x| x % n).min().unwrap();
+        println!("{}-{:?}-{:?}",n, inputArray.iter().map(|&x| x % n).collect::<Vec<i32>>(), inputArray.iter().map(|x| x % n).min() );
+        if 0 != result {
+            jump = n;
+            break;
+        }
+    }
 }
+
 
 #[cfg(test)]
 mod test {
