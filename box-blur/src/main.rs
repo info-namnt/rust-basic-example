b0b547a6f3858cd7f94ce0942b55b72bfb7c7ffe
Nam Nguyen <info.namnt@gmail.com>
2022-05-11 00:49:47 +0700

    add box blur

---
+++ box-blur/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    let image = vec![[1, 1, 1],
+                                 [1, 7, 1],
+                                [1, 1, 1]];
+    println!("{:?}", image);
+}
+
+fn solution(image: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+    let mut result: Vec<Vec<i32>> = Vec::new();
+    let height = image.len();
+    let width = image[0].len();
+
+    for i in 0..height {
+
+    }
+
+    result
+}
